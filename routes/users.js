479074258c60a7c7cd35a7255102e748b487528b
hugogarcia1026 <garcia.hmg10@gmail.com>
2026-03-31 16:55:31 -0300

    buscando usuario

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 const db = require('../db');
 
 routes.get('/', (req, res) => {
-    db.query('SELECT * FROM users', (err, results) => {
+    db.query('SELECT * FROM usuarios', (err, results) => {
         if (err) {
             return res.status(500).json({ error: 'Erro ao buscar usuários' });
         }
@@ -11,46 +11,58 @@ routes.get('/', (req, res) => {
     });
 });
 
+//criar um novo usuário
 routes.post('/create', (req, res) => {
-    const { nome, email, senha } = req.body; 
-    db.query('INSERT INTO usuarios (nome, email, senha) VALUES (?, ?)', 
+    const { nome, email, senha } = req.body;
+    db.query('INSERT INTO usuarios (nome, email, senha) VALUES (?, ?)',
         [nome, email, senha], (err, result) => {
-        if (err) {
-        res.status(500).json({ error: 'Erro ao criar usuário' });
-        } else {
-        res.status(201).json({ id: results.insertId, nome, email });
-        }
-    });
+            if (err) {
+                res.status(500).json({ error: 'Erro ao criar usuário' });
+            } else {
+                res.status(201).json({ id: results.insertId, nome, email });
+            }
+        });
 });
 
 //editar um usuario
 routes.put('/:id', (req, res) => {
-    const { id } = req.params; 
-    const { nome, email, senha } = req.body; 
-    db.query('UPDATE usuarios SET nome = ?, email = ? WHERE id = ?', 
+    const { id } = req.params;
+    const { nome, email, senha } = req.body;
+    db.query('UPDATE usuarios SET nome = ?, email = ? WHERE id = ?',
         [nome, email, id], (err, result) => {
-        if (err) {
-            res.status(500).json({ error: 'Erro ao atualizar usuário' });
-        }else {
-            res.status(201).json({ id, nome, email });
-        }   
-    });
+            if (err) {
+                res.status(500).json({ error: 'Erro ao atualizar usuário' });
+            } else {
+                res.status(201).json({ id, nome, email });
+            }
+        });
 });
 
 //deletar um usuario
 routes.delete('/delete/:id', (req, res) => {
-    const { id } = req.params; 
-
-    db.query('DELETE FROM users WHERE id = ?', [id], (err, result) => {
+    const { id } = req.params;
+    db.query('DELETE FROM usuarios WHERE id = ?', [id], (err, result) => {
         if (err) {
-            return res.status(500).json({ error: 'Erro ao deletar usuário' });
+            res.status(500).json({ error: 'Erro ao deletar usuário' });
+        } else {
+            res.status(201)({ message: 'Usuário deletado com sucesso' });
         }
-        
-        if (result.affectedRows === 0) {
-            return res.status(404).json({ error: 'Usuário não encontrado' });
+    });
+});
+
+//buscar um usuario por id
+routes.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db.query('SELECT * FROM usuarios WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao buscar usuário' });
+        } else {
+            if (results.length === 0) {
+                res.status(404).json({ error: 'Usuário não encontrado' });
+            } else {
+                res.status(201).json(results[0]);
+            }
         }
-        
-        res.json({ message: 'Usuário deletado com sucesso' });
     });
 });
 
