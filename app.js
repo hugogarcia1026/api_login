1e0a441eabd7d36322dd49773c98326b10215405
hugogarcia1026 <garcia.hmg10@gmail.com>
2026-04-09 13:41:47 -0300

    alterando o app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 //Rotas
-const userRoutes = require('./routes/users');
+const userRoutes = require('./routes/corredor');
 const corredorRoutes = require('./routes/corredor');
 app.use('/users', userRoutes);
 app.use('/corredores', corredorRoutes);
--- routes/corredor.js
+++ routes/corredor.js
@@ -5,65 +5,61 @@ const db = require('../db');
 routes.get('/', (req, res) => {
     db.query('SELECT * FROM corredores', (err, results) => {
         if (err) {
-            return res.status(500).json({ error: 'Erro ao buscar corredor' });
+            return res.status(500).json({ error: 'Erro ao buscar corredores' });
         }
         res.json(results);
     });
 });
 
-//criar um novo usuário
 routes.post('/create', (req, res) => {
-    const { nome, turma, } = req.body;
-    db.query('INSERT INTO corredores (nome, turma, ) VALUES (?, ?)',
-        [nome, turma, ], (err, result) => {
+    const { nome, turma } = req.body;
+    db.query('INSERT INTO corredores (nome, turma) VALUES (?, ?)',
+        [nome, turma], (err, result) => {
             if (err) {
                 res.status(500).json({ error: 'Erro ao criar um corredor' });
             } else {
-                res.status(201).json({ id: results.insertId, nome, turma });
+                res.status(201).json({ id: result.insertId, nome, turma });
             }
         });
 });
 
-//editar um usuario
 routes.put('/:id', (req, res) => {
     const { id } = req.params;
-    const { nome, turma, } = req.body;
+    const { nome, turma } = req.body;
     db.query('UPDATE corredores SET nome = ?, turma = ? WHERE id = ?',
         [nome, turma, id], (err, result) => {
             if (err) {
                 res.status(500).json({ error: 'Erro ao atualizar corredor' });
             } else {
-                res.status(201).json({ id, nome, turma });
+                res.status(200).json({ id, nome, turma });
             }
         });
 });
 
-//deletar um usuario
 routes.delete('/delete/:id', (req, res) => {
     const { id } = req.params;
     db.query('DELETE FROM corredores WHERE id = ?', [id], (err, result) => {
         if (err) {
             res.status(500).json({ error: 'Erro ao deletar corredor' });
         } else {
-            res.status(201)({ message: 'Corredor deletado com sucesso' });
+            res.status(200).json({ message: 'Corredor deletado com sucesso' });
         }
     });
 });
 
-//buscar um usuario por id
 routes.get('/:id', (req, res) => {
     const { id } = req.params;
     db.query('SELECT * FROM corredores WHERE id = ?', [id], (err, results) => {
         if (err) {
-            res.status(500).json({ error: 'Erro ao buscar correodor' });
+            res.status(500).json({ error: 'Erro ao buscar corredor' });
         } else {
             if (results.length === 0) {
-                res.status(404).json({ error: 'Coredor não encontrado' });
+                res.status(404).json({ error: 'Corredor não encontrado' });
             } else {
-                res.status(201).json(results[0]);
+                res.status(200).json(results[0]);
             }
         }
     });
 });
 
-module.exports = routes; 
+module.exports = routes;
