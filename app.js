595083b6a3357eeb7a72c34c8fcd2437ef5daef7
hugogarcia1026 <garcia.hmg10@gmail.com>
2026-04-13 08:25:10 -0300

    alterando o código

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express();
 app.use(express.json());
 
 const userRoutes = require('./routes/users');
-//const userRoutes = require('./routes/corredores');
-//const userRoutes = require('./routes/voltas');
+const userRoutes = require('./routes/corredores');
+const userRoutes = require('./routes/voltas');
 app.use('/users', userRoutes);
-//app.use('/corredores', userRoutes);
-//app.use('/voltas', userRoutes);
+app.use('/corredores', userRoutes);
+app.use('/voltas', userRoutes);
 
 
 module.exports = app;
